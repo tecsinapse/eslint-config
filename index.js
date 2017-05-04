b1f3b38271d64fbb3c2dacfa63c2e9c40f41e445
filipe_nevola <filipe.nevola@tecsinapse.com.br>
2017-05-04 15:23:56 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "allowImportExportEverywhere": true
+    },
+    "plugins": [
+        "meteor"
+    ],
+    "extends": [
+        "airbnb",
+        "plugin:meteor/recommended",
+        "prettier",
+        "@meteorjs/eslint-config-meteor"
+    ],
+    "rules": {
+        "import/no-extraneous-dependencies": "off",
+        "import/prefer-default-export": "off",
+        "no-underscore-dangle": "off",
+        "meteor/audit-argument-checks": "off",
+        "no-plusplus": "off",
+        "arrow-parens": "off",
+        "comma-dangle": [
+            "error",
+            {
+                "arrays": "always-multiline",
+                "objects": "always-multiline",
+                "imports": "always-multiline",
+                "exports": "always-multiline",
+                "functions": "never"
+            }
+        ],
+        "no-console": [
+            "warn",
+            {
+                "allow": [
+                    "warn",
+                    "error",
+                    "time",
+                    "timeEnd"
+                ]
+            }
+        ],
+        "object-shorthand": [
+            "error",
+            "always",
+            {
+                "avoidQuotes": false
+            }
+        ],
+        "meteor/eventmap-params": [
+            "error",
+            {
+                "eventParamName": "event",
+                "templateInstanceParamName": "instance"
+            }
+        ],
+        "meteor/template-names": [
+            "off"
+        ]
+    },
+    "settings": {
+        "import/resolver": "meteor"
+    }
+};
