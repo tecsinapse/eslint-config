9b61c6cca197e78bde917282159690ce5c483fb6
Renan Castro <renanccastro@gmail.com>
2018-10-26 16:57:00 -0300

    - Bumping package versions - Adding npm org - Prettier disable rules

--- index.js
+++ index.js
@@ -1,79 +1,72 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "parserOptions": {
-    "allowImportExportEverywhere": true
+  parser: 'babel-eslint',
+  parserOptions: {
+    allowImportExportEverywhere: true,
   },
-  "plugins": [
-    "meteor"
+  extends: [
+    'airbnb',
+    'plugin:meteor/recommended',
+    'prettier',
+    'prettier/react',
+    'react-app',
+    'plugin:import/errors'
   ],
-  "extends": [
-    "airbnb",
-    "plugin:meteor/recommended",
-    "prettier",
-    "@meteorjs/eslint-config-meteor"
-  ],
-  "rules": {
+  plugins: ['react', 'import'],
+  rules: {
     //using prettier
-    "indent": "off",
+    indent: 'off',
     //using prettier
-    "react/jsx-indent": "off",
+    'react/jsx-indent': 'off',
     //using prettier
-    "react/jsx-indent-props": "off",
-    "react/prop-types": "off",
-    "no-confusing-arrow": "off",
-    "jsx-a11y/no-static-element-interactions": "off",
-    "react/jsx-space-before-closing": "off",
-    "react/jsx-tag-spacing": "off",
-    "max-len": "off",
+    'react/jsx-indent-props': 'off',
+    'react/prop-types': 'off',
+    'no-confusing-arrow': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'react/jsx-space-before-closing': 'off',
+    'react/jsx-tag-spacing': 'off',
+    'max-len': 'off',
     //it is not working correctly with ternary operator
-    "react/jsx-closing-bracket-location": "off",
-    "import/no-extraneous-dependencies": "off",
-    "import/prefer-default-export": "off",
-    "import/no-named-default": "off",
-    "no-underscore-dangle": "off",
-    "meteor/audit-argument-checks": "off",
-    "no-plusplus": "off",
-    "arrow-parens": "off",
-    "comma-dangle": [
-      "error",
+    'react/jsx-closing-bracket-location': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-named-default': 'off',
+    'no-underscore-dangle': 'off',
+    'meteor/audit-argument-checks': 'off',
+    'no-plusplus': 'off',
+    'arrow-parens': 'off',
+    'comma-dangle': [
+      'error',
       {
-        "arrays": "always-multiline",
-        "objects": "always-multiline",
-        "imports": "always-multiline",
-        "exports": "always-multiline",
-        "functions": "never"
-      }
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
     ],
-    "no-console": [
-      "warn",
+    'no-console': [
+      'warn',
       {
-        "allow": [
-          "warn",
-          "error",
-          "time",
-          "timeEnd"
-        ]
-      }
+        allow: ['warn', 'error', 'time', 'timeEnd'],
+      },
     ],
-    "object-shorthand": [
-      "error",
-      "always",
+    'object-shorthand': [
+      'error',
+      'always',
       {
-        "avoidQuotes": false
-      }
+        avoidQuotes: false,
+      },
     ],
-    "meteor/eventmap-params": [
-      "error",
+    'meteor/eventmap-params': [
+      'error',
       {
-        "eventParamName": "event",
-        "templateInstanceParamName": "instance"
-      }
+        eventParamName: 'event',
+        templateInstanceParamName: 'instance',
+      },
     ],
-    "meteor/template-names": [
-      "off"
-    ]
+    'meteor/template-names': ['off'],
   },
-  "settings": {
-    "import/resolver": "meteor"
-  }
+  settings: {
+    'import/resolver': 'meteor',
+  },
 };
